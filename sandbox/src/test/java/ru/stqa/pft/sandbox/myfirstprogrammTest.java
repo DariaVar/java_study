925ef49613fa9a7439b38cf970b3de927a4fb5ac
Daria <pologova@exbico.ru>
2019-04-26 17:44:37 +0500

    3 lessons

--- sandbox/src/test/java/ru/stqa/pft/sandbox/myfirstprogrammTest.java
+++ sandbox/src/test/java/ru/stqa/pft/sandbox/myfirstprogrammTest.java
@@ -5,12 +5,13 @@ import org.testng.annotations.Test;
 
 public class myfirstprogrammTest {
 
+
     @Test
 
     public void testDistance () {
         Point p1 = new Point(20,50);
         Point p2 = new Point(55,10);
-        Assert.assertEquals(p1.distance(p1),p2.distance(p2), 53.150729063673246);
+        Assert.assertEquals(p1.distance(p2), 53.150729063673246);
     }
 
 }
