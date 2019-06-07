3431906ede71beba166db1d1d1fb47bd4ed07d95
Daria <pologova@exbico.ru>
2019-06-07 12:26:09 +0500

    20+ реализация просмотров commits github

---
+++ github-sample/src/test/java/ru/stqa/pft/github/GithubTests.java
@@ -0,0 +1,18 @@
+package ru.stqa.pft.github;
+
+import com.google.common.collect.ImmutableMap;
+import com.jcabi.github.*;
+import org.testng.annotations.Test;
+
+import java.io.IOException;
+
+public class GithubTests {
+    @Test
+    public void testCommits() throws IOException {
+        Github github = new RtGithub("8d4dc0f798ff4b33f23235dd9ee1caf39e1d00c3");
+        RepoCommits commits = github.repos().get(new Coordinates.Simple("DariaVar", "java_study")).commits();
+        for (RepoCommit commit : commits.iterate(new ImmutableMap.Builder<String, String>().build())) {
+            System.out.println(new RepoCommit.Smart(commit).message());
+        }
+    }
+}
